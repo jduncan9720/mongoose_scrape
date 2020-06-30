f65e576a89cf88ce36d3c92671bcc4f7dc12cc01
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-06-30 16:29:24 -0600

    working on the scraping

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3002;
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
@@ -27,29 +27,25 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect("mongodb://localhost/nytimes_db", { useNewUrlParser: true, useFindAndModify: false });
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://nytimes.com").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("article").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      result.title = $(element).find("h2").text();
+      result.link = $(element).find("a").attr("href");
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
